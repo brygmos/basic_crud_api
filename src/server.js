ad0ecb4b0fb1641a38ee2a943bd4283259753525
brygmos <daniel-best@yandex.ru>
2023-07-20 01:51:52 +0300

    impl. get user by id; refactor routes

--- src/server.js
+++ src/server.js
@@ -1,48 +1,16 @@
 import { createServer } from 'node:http';
 import userController from "./controllers/userController.js"
-import { usersRoutes } from './routes/usersRoutes.js';
 
 export default function startServer () {
     const server = createServer((req, res) => {
     const { method, url } = req;
-    const controller = new userController
-
-    // const route = usersRoutes.find((route) => {return route.endpoint === url})
-    const route = usersRoutes.find((route) => {return url.startsWith(route.endpoint)})
     if (url.startsWith('/api/users')) {
-        controller.getData(req, res) 
+        userController.getData(req, res) 
     }
     else {
         res.writeHead(400, { 'Content-Type': 'application/json' });
-        res.end('invalid url')
+        res.end('invalid urlll')
     }
-
-//     if (url.startsWith('/api/users/') && method === 'GET') {
-//         let urlLength = url.length;
-//         const id = url.slice(11)
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         res.end(getUser(id))
-//     } else {
-//     	res.statusCode=404;
-//         res.writeHead(404, { 'Content-Type': 'text/plain' });
-//       	res.end('Invalid URL');  
-//    }
-
-
-//     if (url === '/api/users' && method === 'GET') {
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         res.end(getUsers())
-//     }
-//     if (url.startsWith('/api/users/') && method === 'GET') {
-//         let urlLength = url.length;
-//         const id = url.slice(11)
-//         res.writeHead(200, { 'Content-Type': 'application/json' });
-//         res.end(getUser(id))
-//     } else {
-//     	res.statusCode=404;
-//         res.writeHead(404, { 'Content-Type': 'text/plain' });
-//       	res.end('Invalid URL');  
-//    }
 })
 
 server.listen(process.env.PORT, () => console.log(`Server is running on http://localhost:${process.env.PORT}`));
