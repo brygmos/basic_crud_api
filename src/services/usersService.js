baa8422279dc5b80cc52b1fe32cf6b99fc1fcf0e
brygmos <daniel-best@yandex.ru>
2023-07-24 19:41:08 +0300

    full CRUD

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -8,7 +8,8 @@ export default class userService {
     }
     static getUser (id) {
         const user = db.find((user) => {return user.id === id})
-        return user ? JSON.stringify(user) : 'no user'
+        return user ? user : 'no user'
+        // return user ? JSON.stringify(user) : 'no user'
     }
     static postUser (username, age, hobbies) {
         const user = new User(username, age, hobbies)
@@ -22,5 +23,31 @@ export default class userService {
         db.push(user)
         const createdUser = this.getUser(id)
         return createdUser ? createdUser : new Error('unsuccess')
+    }
+    static putUser (id, username, age, hobbies) {
+        const user = this.getUser(id)
+        if (!isUniqueUsername(username)) {
+            throw new Error('username is already used')
+        }
+        username && (user.username = username); 
+        age && (user.age = age);
+        hobbies && (user.hobbies = hobbies);
+        if (user == 'no user') {
+            throw new Error('no user');
+        }
+        const createdUser = this.getUser(id)
+        return createdUser ? createdUser : new Error('unsuccess')
+    }
+    static deleteUser (id) {
+        let user = this.getUser(id)
+        if (user == 'no user') {
+            throw new Error('no user with such ID');
+        }
+        const index = db.findIndex((user) => {return user.id === id})
+        if (index !== -1) {
+            db.splice(index, 1);
+        }
+        user = this.getUser(id)
+        return user == 'no user' ? `user with id ${id} removed successfully` : new Error('unsuccess')
     }
 }
